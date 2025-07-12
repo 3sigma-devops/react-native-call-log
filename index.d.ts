9c56cd2fc135633d1e2e33bac10e053b3e612752
vivek0046 <77613804+vivek0046@users.noreply.github.com>
2025-07-13 02:52:30 +0530

    type issue fix

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,23 @@
-declare namespace CallLogs {
-  export enum callType {
-    OUTGOING = 'OUTGOING',
-    INCOMING = 'INCOMING',
-    MISSED = 'MISSED',
-    VOICEMAIL = 'VOICEMAIL',
-    REJECTED = 'REJECTED',
-    BLOCKED = 'BLOCKED',
-    ANSWERED_EXTERNALLY = 'ANSWERED_EXTERNALLY',
-    UNKNOWN = 'UNKNOWN',
-  }
+export type CallType = 'INCOMING' | 'OUTGOING' | 'MISSED' | 'REJECTED' | 'BLOCKED' | 'VOICEMAIL';
 
-  export interface CallFilter {
-    minTimestamp?: number;
-    maxTimestamp?: number;
-    types?: CallType | CallType[];
-    phoneNumbers?: string | string[];
-  }
-
-  export interface CallLog {
-    phoneNumber: string;
-    duration: number;
-    name: string;
-    timestamp: string;
-    dateTime: string;
-    type: CallType;
-    rawType: number;
-  }
-
-  const load: (limit: number, filter?: CallFilter) => Promise<CallLog[]>;
-
-  const loadAll: () => Promise<CallLog[]>;
+export interface CallLogItem {
+  phoneNumber: string;
+  name?: string;
+  timestamp: number;
+  duration: number;
+  callType: CallType;
 }
 
-export = CallLogs;
+export interface CallLogFilter {
+  minTimestamp?: number;
+  maxTimestamp?: number;
+  types?: CallType[] | CallType;
+  phoneNumbers?: string[] | string;
+}
+
+declare class CallLogs {
+  static load(limit: number, filter?: CallLogFilter): Promise<CallLogItem[]>;
+  static loadAll(): Promise<CallLogItem[]>;
+}
+
+export default CallLogs;
